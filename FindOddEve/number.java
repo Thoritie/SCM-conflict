1a7d570439df47bd6e6a75bc713163604af78567
DELL <DELL@DESKTOP-GBQKCCC>
2017-09-14 09:40:48 +0700

    main class :girls:

--- FindOddEve/number.java
+++ FindOddEve/number.java
@@ -1,9 +1,21 @@
 
 public class number {
-
+	import java.util.Scanner;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
+	
+		Scanner in = new Scanner(System.in);
+		System.out.println(“Input number”);
+		int number,num;
+		
+		
+//		number =in.nextInt(); 
+//		num= number%2;
+//
+//		if (num ==0) 
+//		System.out.println(“àÅ¢¤Ùè”);
+//		else
+//		System.out.println(“àÅª¤Õè”);
+//		}
 
 }
